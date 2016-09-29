41d58b0d783896bdc65648f363fb9319ab4b85db
cunshuifengyun <wangshixiebo@126.com>
2016-09-29 20:45:27 +0800

    add new song

--- demo.js
+++ demo.js
@@ -13,6 +13,12 @@ var ap4 = new APlayer({
             author: '未知',
             url: '../resource/music/太阳的后裔合集.mp3',
             pic: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3570221259,4023704566&fm=58'
-        }
+        },
+		{
+            title: '滚滚红尘',
+            author: '陈淑桦',
+            url: 'http://oe9nwsvcl.bkt.clouddn.com/%E6%BB%9A%E6%BB%9A%E7%BA%A2%E5%B0%98%20%E9%99%88%E6%B7%91%E6%A1%A6.mp3'
+        },
+		
     ]
 });
