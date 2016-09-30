23b0e717eb367976798e8ea68025ede4cd0d3ff1
cunshuifengyun <wangshixiebo@126.com>
2016-09-30 10:36:01 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -14,7 +14,7 @@ var ap4 = new APlayer({
             url: 'http://oe9nwsvcl.bkt.clouddn.com/%E5%A4%AA%E9%98%B3%E7%9A%84%E5%90%8E%E8%A3%94%E5%90%88%E9%9B%86.mp3',
             pic: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3570221259,4023704566&fm=58'
         },
-		{
+	{
             title: '滚滚红尘',
             author: '陈淑桦',
             url: 'http://oe9nwsvcl.bkt.clouddn.com/%E6%BB%9A%E6%BB%9A%E7%BA%A2%E5%B0%98%20%E9%99%88%E6%B7%91%E6%A1%A6.mp3'
