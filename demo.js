3f7a962a36e72f55cd89d63d0ae16899656923ff
cunshuifengyun <wangshixiebo@126.com>
2016-09-30 14:34:49 +0800

    add new song

--- demo.js
+++ demo.js
@@ -19,6 +19,16 @@ var ap4 = new APlayer({
             author: '陈淑桦',
             url: 'http://oe9nwsvcl.bkt.clouddn.com/%E6%BB%9A%E6%BB%9A%E7%BA%A2%E5%B0%98%20%E9%99%88%E6%B7%91%E6%A1%A6.mp3'
         },
+		{
+			title: '笑红尘',
+			author: '陈淑桦',
+			url: 'http://oe9nwsvcl.bkt.clouddn.com/%E9%99%88%E6%B7%91%E6%A1%A6%20-%20%E7%AC%91%E7%BA%A2%E5%B0%98.mp3'
+		},
+		{
+			title: '秋水悠悠',
+			author: '巫娜',
+			url: 'http://oe9nwsvcl.bkt.clouddn.com/%E5%B7%AB%E5%A8%9C%20-%20%E7%A7%8B%E6%B0%B4%E6%82%A0%E6%82%A0.mp3'
+		}
 		
     ]
 });
